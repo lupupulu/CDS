c36ea3a7ca03c9caaed124688ae54f26f74ec149
lupupulu <lupu_lupu@outlook.com>
2025-07-12 15:39:21 +0800

    Add CCMD_NEXT defined.

--- ccmd.h
+++ ccmd.h
@@ -2,12 +2,13 @@
 #define _CCMD_H
 
 void ccmd_init(int argc,const char **argv);
-#define CCMD_REQUIRED  0b00000
-#define CCMD_OPTIONAL  0b00001
-#define CCMD_STR_PARAM 0b00010
-#define CCMD_INT_PARAM 0b00100
-#define CCMD_VARIABLE  0b01000
-#define CCMD_FUNCTION  0b10000
+#define CCMD_REQUIRED  0b000000
+#define CCMD_OPTIONAL  0b000001
+#define CCMD_STR_PARAM 0b000010
+#define CCMD_INT_PARAM 0b000100
+#define CCMD_VARIABLE  0b001000
+#define CCMD_FUNCTION  0b010000
+#define CCMD_NEXT      0b100000
 typedef void(*CCMD_FUNC)(void);
 void ccmd_set_para(const char *sw,int flg,void *data);
 /// @param exit if param unknown,0:continue the function;1:break the function.
